b67a2826ef97eed6e50491529874c44b6971303a
Michael Stark <michael.stark@sportdeutschland.tv>
2019-06-27 11:46:19 +0200

    updates scope syntax

--- lib/vidibus/versioning/version.rb
+++ lib/vidibus/versioning/version.rb
@@ -5,22 +5,22 @@ module Vidibus
       include ::Mongoid::Timestamps
       include Vidibus::Uuid::Mongoid
 
-      belongs_to :versioned, :polymorphic => true
+      belongs_to :versioned, polymorphic: true
 
-      field :versioned_uuid, :type => String
-      field :versioned_attributes, :type => Hash, :default => {}
-      field :number, :type => Integer
+      field :versioned_uuid, type: String
+      field :versioned_attributes, type: Hash, default: {}
+      field :number, type: Integer
 
       index({versioned_uuid: 1, number: 1})
 
-      validates :versioned_uuid, :versioned_attributes, :number, :presence => true
-      validates :number, :uniqueness => {
-        :scope => [:versioned_id, :versioned_type]
+      validates :versioned_uuid, :versioned_attributes, :number, presence: true
+      validates :number, uniqueness: {
+        scope: [:versioned_id, :versioned_type]
       }
 
       before_validation :set_number, :set_versioned_uuid
 
-      scope :timeline, desc(:created_at)
+      scope :timeline, -> { desc(:created_at) }
 
       def past?
         !!(created_at && created_at < Time.now)
@@ -36,8 +36,8 @@ module Vidibus
         return if number
         previous = Version.
           where({
-            :versioned_id => versioned_id,
-            :versioned_type => versioned_type
+            versioned_id: versioned_id,
+            versioned_type: versioned_type
           }).
           desc(:number).limit(1).first
         self.number = previous ? previous.number + 1 : 1
